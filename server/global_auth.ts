383e279cc515ff786bd2e91a25a9e8d1c22383b9
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2025-05-19 23:58:40 +0200

    Added GlobalAuthentication method

---
+++ server/global_auth.ts
@@ -0,0 +1,25 @@
+import fs from 'fs'
+import path from "path";
+import { __dirname } from "./utils.ts";
+var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf-8'))
+
+export const globalAuthentication = (req, res, next) => {
+    const authheader = req.headers.authorization;
+    if (!authheader) {
+        res.setHeader('WWW-Authenticate', 'Basic realm="mon site à moi", charset="UTF-8"');
+        res.status(401).end();
+    }
+    else {
+      /*extraire la partie credentials de la propriété "authorization", en en supprimant la partie Basic située au début, décoder le Base64 */
+      const credentials  = Buffer.from(authheader.split(' ')[1],'base64').toString();
+      const [user,password] = credentials.split(':');
+      if (user === config.server.globalAuthentication.username && password === config.server.globalAuthentication.password) {
+          // let's continue
+          next();
+      } else {
+        // mauvais user name ou password
+        res.setHeader('WWW-Authenticate', 'Basic realm="mon site à moi", charset="UTF-8"');
+        res.status(401).end();
+      }
+    }
+}
