28561632965850140ae19c1ff120ad0bbb3d40eb
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2025-05-20 08:23:05 +0200

    authentication fallback when Bearer is set

--- server/global_auth.ts
+++ server/global_auth.ts
@@ -1,9 +1,14 @@
 import fs from 'fs'
 import path from "path";
 import { __dirname } from "./utils.ts";
+import { authenticateToken } from "./login.ts";
 var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf-8'))
 
 export const globalAuthentication = (req, res, next) => {
+    if(!config.server.globalAuthentication.enabled) {
+        next();
+        return;
+    }
     const authheader = req.headers.authorization;
     if (!authheader) {
         res.setHeader('WWW-Authenticate', 'Basic realm="mon site à moi", charset="UTF-8"');
@@ -11,6 +16,10 @@ export const globalAuthentication = (req, res, next) => {
     }
     else {
       /*extraire la partie credentials de la propriété "authorization", en en supprimant la partie Basic située au début, décoder le Base64 */
+      if(authheader && authheader.split(' ')[0] == "Bearer") {
+        authenticateToken(req, res, next);
+        return;
+      }
       const credentials  = Buffer.from(authheader.split(' ')[1],'base64').toString();
       const [user,password] = credentials.split(':');
       if (user === config.server.globalAuthentication.username && password === config.server.globalAuthentication.password) {
