730dff466548e4201e14c7b0ee3f3dcdd3aea0e9
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2025-05-27 09:13:54 +0200

    added test user in database

--- server/database_init.ts
+++ server/database_init.ts
@@ -1,11 +1,14 @@
 import sqlite3 from "better-sqlite3";
 import path from "path";
+import bcrypt from "bcrypt";
 import { __dirname } from "./utils.ts";
+import fs from 'fs'
+var config = JSON.parse(fs.readFileSync(path.join(__dirname, 'config.json'), 'utf-8'))
 
 // Create a new database or open an existing one
 export const db = new sqlite3(path.join(__dirname, "database.db"));
 
-export const database_init = () => {
+export const database_init = async () => {
     try {
         db.exec(
             `CREATE TABLE IF NOT EXISTS users (
@@ -59,6 +62,16 @@ export const database_init = () => {
             CREATE UNIQUE INDEX IF NOT EXISTS idx_userId ON tokens (userId);
         `);
         console.log("Database schema initialized successfully.");
+        if(config.addTestUser){
+            const salt = await bcrypt.genSalt(Number(config.salt));
+            const hashedPassword = await bcrypt.hash("test", salt);
+            const stmt = db.prepare(`
+                INSERT OR IGNORE INTO users (username, firstname, lastname, email, password, verified)
+                VALUES (?, ?, ?, ?, ?, ?)
+            `);
+            stmt.run("test", "Test", "User", "", hashedPassword, 1);
+            console.log("Test user added successfully.");
+        }
     } catch (err) {
         console.error("Error initializing database schema:", err.message);
     }
