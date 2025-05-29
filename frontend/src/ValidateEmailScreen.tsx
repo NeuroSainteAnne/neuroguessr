1bb666c71549eb011aa987244ac2670ad6875dde
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2025-05-29 09:33:56 +0200

    update email validation logic on frontend

--- frontend/src/ValidateEmailScreen.tsx
+++ frontend/src/ValidateEmailScreen.tsx
@@ -1,10 +1,67 @@
 import type { TFunction } from 'i18next';
 import './ValidateEmailScreen.css'
+import { useEffect, useState } from 'react';
 
 function ValidateEmailScreen({t, callback}:{ t: TFunction<"translation", undefined>, callback: AppCallback }) {
+    const [isValidatedEmail, setIsValidatedEmail] = useState<boolean>(false);
+    const [validatedErrorText, setValidatedErrorText] = useState<string>("");
+    const [validatedSuccessText, setValidatedSuccessText] = useState<string>("");
+
+    useEffect(() => {
+        handleValidateEmail()
+    }, [])
+
+    const handleValidateEmail = async () => {
+        // Extract token from URL
+        const hash = window.location.hash.replace(/^#\/?/, "");
+        const parts = hash.split("/");
+        const userId = parts[1];
+        const token = parts[2];
+                
+        if (!token || !userId) {
+          setValidatedErrorText(t('error_invalid_token'));
+        }  
+        try {
+          const response = await fetch('/api/verify-email', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ id: userId, token }),
+          });
+
+          const result = await response.json();
+
+          if (!response.ok) {
+            setValidatedErrorText(t('error_invalid_token'));
+          } else {
+            setValidatedSuccessText(t('success_email_verified'));
+            setTimeout( () => {
+              callback.loginWithToken(result.token);
+              callback.gotoPage("welcome");
+            }, 1000);
+          }
+        } catch (error) {
+          console.error('Error validating token:', error);
+          setValidatedErrorText(t('server_error'))
+        }
+    }
+    
   return (
     <>
-        <div>Placeholder for email validation</div>
+    <div className="page-container">
+          <div className="register-box">
+              <h2>{t("validate_email_header")}</h2>
+              <table className="login-element">
+                  {!validatedErrorText && !validatedSuccessText &&
+                    <tr><td colSpan={2} className="recovery-message">{t("email_validation_wait")}</td></tr>}
+                  {validatedErrorText &&
+                    <tr><td colSpan={2} id="recovery_error" className="recovery-message">{validatedErrorText}</td></tr>}
+                  {validatedSuccessText &&
+                    <tr><td colSpan={2} id="recovery_success" className="recovery-message">{validatedSuccessText}</td></tr>}
+              </table>
+          </div>
+    </div>
     </>
   )
 }
