35abcd4942ef548cef7fdeb4c881e09f8757a25e
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2025-05-31 14:01:19 +0200

    refactored multi backend

--- backend/modules/multi.ts
+++ backend/modules/multi.ts
@@ -19,15 +19,17 @@ interface WSGame extends WebSocket {
   sessionCode?: string;
 } 
 
+interface MultiplayerParametersType {
+  atlas?: string
+  regionsNumber: number;
+  durationPerRegion: number;
+  gameoverOnError: boolean;
+}
+
 interface MultiplayerGame {
   lobby: Set<WebSocket>;
   hasStarted: boolean;
-  parameters: {
-    atlas?: string
-    regionsNumber: number;
-    durationPerRegion: number;
-    gameoverOnError: boolean;
-  }
+  parameters: MultiplayerParametersType
 } 
 
 const app = express();
@@ -64,106 +66,117 @@ export const createMultiplayerSession = async (req: Request, res: Response) => {
     });
 };
 
+function joinLobby(ws: WebSocket, usertoken: string, sessionCode: string) {
+  let jwtpayload: any;
+  try {
+    jwtpayload = jwt.verify(usertoken, config.jwt_secret);
+  } catch (err) {
+    ws.send(JSON.stringify({ type: 'error', message: 'Invalid or expired token.' }));
+    ws.close();
+    return;
+  }
+  const userName = jwtpayload.username;
+  (ws as WSGame).userName = userName;
+  const session = db.prepare("SELECT * FROM multisessions WHERE sessionCode = ?").get(sessionCode);
+  if (!session) {
+    ws.send(JSON.stringify({ type: 'error', message: 'Lobby does not exist.' }));
+    ws.close();
+    return;
+  }
+  if (!games[sessionCode]){
+    games[sessionCode] = {
+      lobby: new Set(),
+      hasStarted: false,
+      parameters: {
+        regionsNumber: DEFAULT_REGION_NUMBER,
+        durationPerRegion: DEFAULT_DURATION_PER_REGION,
+        gameoverOnError: DEFAULT_GAMEOVER_ON_ERROR
+      }
+    }
+  }
+  const gameRef = games[sessionCode];
+  gameRef.lobby.add(ws);
+  (ws as WSGame).gameRef = gameRef;
+  (ws as WSGame).sessionCode = sessionCode;
+  
+  // Send the list to the newly joined client
+  const userList = Array.from(gameRef.lobby)
+    .map(client => (client as any).userName)
+    .filter(Boolean);
+  ws.send(JSON.stringify({ type: 'lobby-users', users: userList }));
+  ws.send(JSON.stringify({ type: 'parameters-updated', parameters: gameRef.parameters }));
+
+  // Broadcast to others in the lobby
+  gameRef.lobby.forEach(client => {
+    if (client !== ws && client.readyState === ws.OPEN) {
+      client.send(JSON.stringify({ type: 'player-joined', userName: userName }));
+    }
+  });
+}
+
+function launchGame(ws: WebSocket, sessionToken: string){
+  const lobby = (ws as WSGame).gameRef?.lobby;
+  const userName = (ws as WSGame).userName;
+  const sessionCode = (ws as WSGame).sessionCode
+  if (!lobby || !userName || !sessionCode || !Array.from(lobby).some(client => (client as any).userName === userName)) {
+    ws.send(JSON.stringify({ type: 'error', message: 'You are not in the lobby.' }));
+    return;
+  }
+  const gameRef = games[sessionCode];
+  // Check that the sessionToken matches the one in the multisessions table
+  const session = db.prepare("SELECT sessionToken FROM multisessions WHERE sessionCode = ?").get(sessionCode) as { sessionToken: string };
+  if (!session || session.sessionToken !== sessionToken) {
+    ws.send(JSON.stringify({ type: 'error', message: 'Invalid session token for this lobby.' }));
+    return;
+  }
+  if (gameRef.hasStarted) {
+    ws.send(JSON.stringify({ type: 'error', message: 'Game already started.' }));
+    return;
+  }
+  console.log("Starting game", sessionCode)
+  gameRef.hasStarted = true;
+  // broadcast gamestart to all users
+  gameRef.lobby.forEach(client => {
+    if (client.readyState === ws.OPEN) {
+      client.send(JSON.stringify({ type: 'game-start' }));
+    }
+  });
+}
+
+function updateParameters(ws: WebSocket, parameters: MultiplayerParametersType) {
+  // Update the game parameters for this lobby
+  const gameRef = (ws as WSGame).gameRef;
+  if (!gameRef) {
+    ws.send(JSON.stringify({ type: 'error', message: 'Game not available.' }));
+    return;
+  }
+  gameRef.parameters = {
+    ...gameRef.parameters,
+    ...parameters
+  };
+  // Broadcast updated parameters to all lobby members
+  gameRef.lobby.forEach(client => {
+    if (client.readyState === ws.OPEN) {
+      client.send(JSON.stringify({ type: 'parameters-updated', parameters: gameRef.parameters }));
+    }
+  });
+}
+
 wss.on('connection', (ws, req) => {
-  // Expect the client to send a JSON message with { type: 'join', sessionCode: '12345678', userId: ... }
   ws.on('message', (message) => {
     try {
       const data = JSON.parse(message.toString());
       if (data.type === 'join' && data.sessionCode && data.token) {
-        let jwtpayload: any;
-        try {
-          jwtpayload = jwt.verify(data.token, config.jwt_secret);
-        } catch (err) {
-          ws.send(JSON.stringify({ type: 'error', message: 'Invalid or expired token.' }));
-          ws.close();
-          return;
-        }
-        const userName = jwtpayload.username;
-        (ws as WSGame).userName = userName;
-        const session = db.prepare("SELECT * FROM multisessions WHERE sessionCode = ?").get(data.sessionCode);
-        if (!session) {
-          ws.send(JSON.stringify({ type: 'error', message: 'Lobby does not exist.' }));
-          ws.close();
-          return;
-        }
-        if (!games[data.sessionCode]){
-          games[data.sessionCode] = {
-            lobby: new Set(),
-            hasStarted: false,
-            parameters: {
-              regionsNumber: DEFAULT_REGION_NUMBER,
-              durationPerRegion: DEFAULT_DURATION_PER_REGION,
-              gameoverOnError: DEFAULT_GAMEOVER_ON_ERROR
-            }
-          }
-        }
-        games[data.sessionCode].lobby.add(ws);
-        (ws as WSGame).gameRef = games[data.sessionCode];
-        (ws as WSGame).sessionCode = data.sessionCode;
-        
-        // Send the list to the newly joined client
-        const userList = Array.from(games[data.sessionCode].lobby)
-          .map(client => (client as any).userName)
-          .filter(Boolean);
-        ws.send(JSON.stringify({ type: 'lobby-users', users: userList }));
-        ws.send(JSON.stringify({ type: 'parameters-updated', parameters: games[data.sessionCode].parameters }));
-
-        // Broadcast to others in the lobby
-        games[data.sessionCode].lobby.forEach(client => {
-          if (client !== ws && client.readyState === ws.OPEN) {
-            client.send(JSON.stringify({ type: 'player-joined', userName: userName }));
-          }
-        });
+        joinLobby(ws, data.token, data.sessionCode)
       } else if (data.type === 'launch-game' && data.sessionToken) {
-        const lobby = (ws as WSGame).gameRef?.lobby;
-        const userName = (ws as WSGame).userName;
-        const sessionCode = (ws as WSGame).sessionCode
-        if (!lobby || !userName || !Array.from(lobby).some(client => (client as any).userName === userName)) {
-          ws.send(JSON.stringify({ type: 'error', message: 'You are not in the lobby.' }));
-          return;
-        }
-        // Check that the sessionToken matches the one in the multisessions table
-        const session = db.prepare("SELECT sessionToken FROM multisessions WHERE sessionCode = ?").get(sessionCode) as {sessionToken: string};
-        if (!session || session.sessionToken !== data.sessionToken) {
-          ws.send(JSON.stringify({ type: 'error', message: 'Invalid session token for this lobby.' }));
-          return;
-        }
-        if(games[data.sessionCode].hasStarted){
-          ws.send(JSON.stringify({ type: 'error', message: 'Game already started.' }));
-          return;
-        }
-        console.log("Starting game", sessionCode)
-        games[data.sessionCode].hasStarted = true;
-        // broadcast gamestart to all users
-        games[data.sessionCode].lobby.forEach(client => {
-          if (client.readyState === ws.OPEN) {
-            client.send(JSON.stringify({ type: 'game-start' }));
-          }
-        });
+        launchGame(ws, data.sessionToken)
       } else if (data.type === 'update-parameters' && (ws as WSGame).gameRef && typeof data.parameters === 'object') {
-        // Update the game parameters for this lobby
-        const gameRef = (ws as WSGame).gameRef;
-        if(!gameRef){
-          ws.send(JSON.stringify({ type: 'error', message: 'Game not available.' }));
-          return;
-        }
-        gameRef.parameters = {
-          ...gameRef.parameters,
-          ...data.parameters
-        };
-        // Optionally broadcast updated parameters to all lobby members
-        gameRef.lobby.forEach(client => {
-          if (client.readyState === ws.OPEN) {
-            client.send(JSON.stringify({ type: 'parameters-updated', parameters: gameRef.parameters }));
-          }
-        });
+        updateParameters(ws, data.parameters)
       }
     } catch (e) {
       ws.send(JSON.stringify({ type: 'error', message: e }));
     }
   });
-
-  
 
   ws.on('close', () => {
     // Remove from current lobby
@@ -184,5 +197,5 @@ wss.on('connection', (ws, req) => {
 });
 
 server.listen(3001, () => {
-  console.log('Server and WebSocket running on port 3001');
+  console.log('WebSocket running on port 3001');
 });
