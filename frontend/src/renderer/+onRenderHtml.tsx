c671215e4b98f8d4c5398a96fc74606bbbef1dc0
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2025-06-09 17:50:41 +0200

    updated google tag manager

--- frontend/src/renderer/+onRenderHtml.tsx
+++ frontend/src/renderer/+onRenderHtml.tsx
@@ -37,17 +37,17 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext) => {
  const description = t((pageContext.config as any).description || 'neuroguessr_short_description', { lng: language })
  const image = (pageContext.config as any).image || neuroGuessrImage
 
- const gtm = config.googleTagManager ? dangerouslySkipEscape(`<!-- Google Tag Manager -->
-  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-  })(window,document,'script','dataLayer','${config.googleTagManager}');</script>
-  <!-- End Google Tag Manager -->`) : escapeInject``;
- const gtm2 = config.googleTagManager ? dangerouslySkipEscape(`<!-- Google Tag Manager (noscript) -->
-  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=${config.googleTagManager}"
-  height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-  <!-- End Google Tag Manager (noscript) -->`) : escapeInject``;
+ const gtm = config.googleTagManager ? dangerouslySkipEscape(`
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=${config.googleTagManager}"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', '${config.googleTagManager}');
+</script>
+>`) : escapeInject``;
 
  // Create the complete HTML document
   return escapeInject`<!DOCTYPE html>
@@ -238,7 +238,6 @@ const onRenderHtml: OnRenderHtmlAsync = async (pageContext) => {
 </head>
 
 <body>
-  ${gtm2}
   <div id="loading-screen">
     <div class="loader-container">
       <div class="loading-logo-container">
