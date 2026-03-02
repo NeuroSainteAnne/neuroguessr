4729df3a4156cc742d8cb68354364b9db737d2a4
jbenzakoun <28443226+jbenzakoun@users.noreply.github.com>
2026-03-02 20:22:02 +0100

    augmented rate limit for SFNR

--- backend/server.tsx
+++ backend/server.tsx
@@ -69,8 +69,8 @@ if(config.server.globalAuthentication.enabled){
 
 // login.ts
 const authLimiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 15 minutes
-  max: 15, // limit each IP to 5 failed requests per windowMs
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 30, // limit each IP to 30 failed requests per windowMs
   message: 'Too many failed authentication attempts',
   standardHeaders: true,
   legacyHeaders: false,
